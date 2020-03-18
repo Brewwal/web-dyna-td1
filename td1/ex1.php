d7106a73f22043b0c46baee22fcb0a39c302fcd1
unknown <21703611@etu.unicaen.fr>
2020-03-18 14:15:03 +0100

    exercice 1 fini

---
+++ td1/ex1.php
@@ -0,0 +1,6 @@
+<a href="/web-dyna-td1/td1/ex1.php?message=Hello">Afficher Hello</a>
+<a href="/web-dyna-td1/td1/ex1.php?message=Bonjour">Afficher Bonjour</a>
+<a href="/web-dyna-td1/td1/ex1.php?message=Wesh">Afficher Wesh</a>
+
+<?php
+echo "Le lien vous dit ". $_GET["message"];
