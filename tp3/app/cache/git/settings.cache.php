21245d564537d5ce1ccaca6c12363945e23cfe17
Brewwal <21703611@etu.unicaen.fr>
2020-05-07 14:02:43 +0200

    création tp3

---
+++ tp3/app/cache/git/settings.cache.php
@@ -0,0 +1,2 @@
+<?php
+return array("name"=>"web-dyna-td1","remoteUrl"=>"https://github.com/dyna-web-rt/web-dyna-td1","baseFolder"=>"C:\\xampp\\htdocs\\web-dyna-td1","user"=>"Brewwal","password"=>"caca1265ù*\$");
